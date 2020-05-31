6a73751517650a3807f4c01c2a2485dea227bec7
Maxim Gelfand <gelfandmaxim@gmail.com>
2020-05-31 16:51:41 +0300

    Added instructions for playing mystery song

--- library/Parser.hs
+++ library/Parser.hs
@@ -33,3 +33,5 @@ mystery = ".25G3 .25A3 .25C4 .25A3 .5E4 .25p .5E4 .25p .75D4 .75p "
        <> ".25G3 .25A3 .25C4 .25A3 .5E4 .25p .5E4 .25p .75D4 .75p "
        <> ".25G3 .25A3 .25C4 .25A3 .5D5 .25p .5B3 .25p .75C4 .75p "
        <> ".25G3 .25A3 .25C4 .25A3 1C4 .5D4 .2A3 .3B3 .5A3 .5G3 .5p .5G3 .2C4 .8D4 1C4 1p "
+
+-- how to play: play $ volume 30 $ playTups $ parseNotes mystery
