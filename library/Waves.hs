c8356269ef85ede2cd2fe87aab7a82b7af70aeed
Maxim Gelfand <gelfandmaxim@gmail.com>
2020-05-31 15:30:03 +0300

    Can play lists of notes (seven nation army example)

--- library/Waves.hs
+++ library/Waves.hs
@@ -3,32 +3,60 @@ module Waves where
 import           Notes
 import           Numeric                        ( showFFloat )
 
-data Point = Point Float Float
 
-sampleRate = 48000 :: Int
-step = 1 / fromIntegral sampleRate :: Float
+bpm :: Float
+bpm = 120.0
 
 mapPoint :: (Float -> a) -> Point -> (a, a)
 mapPoint f (Point x y) = (f x, f y)
 
-showFullPrecision :: Float -> String
-showFullPrecision x = showFFloat Nothing x ""
-
 sinWave :: Anchor -> Note -> [Float]
-sinWave anchor note =
-  let frequency = freq anchor note
-      xs        = [0, step ..]
-  in  map (sin . (* (2 * pi * frequency))) xs
+sinWave anchor note = map (sin . (* (2 * pi * freq anchor note))) [0, step ..]
 
 sinWave' :: Note -> [Float]
 sinWave' = sinWave defAnchor
-
-test :: Note -> [(String, String)]
-test note =
-  map (mapPoint showFullPrecision) $ zipWith Point [0, step ..] $ sinWave defAnchor note
 
 volume :: Float -> [Float] -> [Float]
 volume vol = map (* (vol / 100))
 
 duration :: Float -> [Float] -> [Float]
-duration = take . round . (* 48000.0)
+duration = take . round . (* (48000.0 * 60.0 / bpm))
+
+playTups :: [(Float, Note)] -> [Float]
+playTups = concatMap (\(dur, note) -> duration dur $ sinWave defAnchor note)
+
+sevenNationArmy = playTups
+  [ (1.5 , Note A nat 2)
+  , (0.5 , Note A nat 2)
+  , (0.75, Note C nat 3)
+  , (0.75, Note A nat 2)
+  , (0.5 , Note G nat 2)
+  , (2.0 , Note F nat 2)
+  , (2.0 , Note E nat 2)
+  , (1.5 , Note A nat 2)
+  , (0.5 , Note A nat 2)
+  , (0.75, Note C nat 3)
+  , (0.75, Note A nat 2)
+  , (0.5 , Note G nat 2)
+  , (0.75, Note F nat 2)
+  , (0.75, Note G nat 2)
+  , (0.5 , Note F nat 2)
+  , (2.0 , Note E nat 2)
+  ]
+
+
+
+
+
+data Point = Point Float Float
+
+sampleRate = 48000 :: Int
+step = 1 / fromIntegral sampleRate :: Float
+
+showFullPrecision :: Float -> String
+showFullPrecision x = showFFloat Nothing x ""
+
+test :: Note -> [(String, String)]
+test note =
+  map (mapPoint showFullPrecision) $ zipWith Point [0, step ..] $ sinWave defAnchor note
+
